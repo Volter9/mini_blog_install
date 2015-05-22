4800f49936d54595b617cba50ce2a6d521475c5c
volter9 <volter925@gmail.com>
2015-05-21 18:38:59 -0700

    Changed the styles of steps links

--- install/html/blocks/header.php
+++ install/html/blocks/header.php
@@ -8,9 +8,12 @@
     
     <div class="right" id="steps">
         <ul>
-        <?php foreach (lang('steps') as $route => $field): ?>
+        <?php $i = 1; foreach (lang('steps') as $route => $field): ?>
             <li class="<?php echo $step > 0 ? 'completed' : '' ?>">
-                <a href="<?php echo url("?route=$route") ?>"><?php echo $field; $step -- ?></a>
+                <a href="<?php echo url("?route=$route") ?>">
+                    <?php echo $i ?>. 
+                    <?php echo $field; $step --; $i ++; ?> 
+                </a>
             </li>
         <?php endforeach; ?>
         </ul>
